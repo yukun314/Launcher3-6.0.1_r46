e7148c1932b4797e885ac7218a7e3c3490bab05c
zyk <yukun314@126.com>
2016-07-04 22:29:04 +0800

    init

--- src/com/zyk/launcher3/setting/HelpFeedbackActivity.java
+++ src/com/zyk/launcher3/setting/HelpFeedbackActivity.java
@@ -1,13 +1,17 @@
 package com.zyk.launcher3.setting;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
 import com.zyk.launcher3.R;
 
 /**
@@ -16,6 +20,7 @@ import com.zyk.launcher3.R;
 public class HelpFeedbackActivity extends Activity {
 
     WebView mWebView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,22 +39,62 @@ public class HelpFeedbackActivity extends Activity {
         return super.onKeyDown(keyCode, event);
     }
 
-    private void setWebView(){
+    private void setWebView() {
         WebSettings ws = mWebView.getSettings();
         ws.setJavaScriptEnabled(true);
-        ws.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+//        if(isNetworkAvailable() | isWifiConnected()){
+//            ws.setCacheMode(WebSettings.LOAD_DEFAULT);
+//        }else {
+//            ws.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+//        }
+
+        //FIXME 为了测试方便 暂时使用只使用网络
+        ws.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        // 开启 DOM storage API 功能
+        ws.setDomStorageEnabled(true);
+        // 开启 database storage API 功能
+        ws.setDatabaseEnabled(true);
+        // 开启 Application Caches 功能
+        ws.setAppCacheEnabled(true);
+
+        //设置适应屏幕
+        ws.setUseWideViewPort(true);
+        ws.setLoadWithOverviewMode(true);
+        ws.setSupportZoom(true);
+        ws.setBuiltInZoomControls(true);
+        ws.setDisplayZoomControls(false);
 
         mWebView.setWebViewClient(new MyWebViewClient());
         mWebView.setWebChromeClient(new MyWebChromeClient());
+        mWebView.setBackgroundColor(Color.argb(0, 0, 0, 0));
     }
 
+    /**
+     * 判断是否有网络连接
+     * @return
+     */
+    private boolean isNetworkAvailable() {
+        ConnectivityManager cm = (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        return (info != null && info.isConnected());//isAvailable
+    }
 
-	/**
+    //是否连接WIFI
+    private boolean isWifiConnected() {
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifiNetworkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (wifiNetworkInfo.isConnected()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
      * 帮助 WebView 处理各种通知、请求事件等
      */
     private class MyWebViewClient extends WebViewClient {
 
-//        如果页面中链接,如果希望点击链接继续在当前browser中响应,而不是新开Android的系统browser中响应该链接,必须覆盖 WebView的WebViewClient对象.
+        //        如果页面中链接,如果希望点击链接继续在当前browser中响应,而不是新开Android的系统browser中响应该链接,必须覆盖 WebView的WebViewClient对象.
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
@@ -58,7 +103,7 @@ public class HelpFeedbackActivity extends Activity {
         }
     }
 
-	/**
+    /**
      * 辅助 WebView 处理 Javascript 的对话框,网站图标,网站 title
      */
     private class MyWebChromeClient extends WebChromeClient {
